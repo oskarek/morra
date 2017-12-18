86e7577513e9f6d5b62966d68a214fd2007fcb04
Oskar Ek <oskarek24@gmail.com>
2017-12-18 11:53:21 +0100

    play against computer done

--- app/Main.hs
+++ app/Main.hs
@@ -1,17 +1,33 @@
 module Main where
 
+import           Control.Monad.Trans.State
 import           Morra
+import           System.IO
 
 main :: IO ()
 main = do
+  hSetBuffering stdout NoBuffering
   printWelcomeMsg
+  endState <- execStateT game (GameState 0 0 1)
+  printWinMsg endState
 
 printWelcomeMsg :: IO ()
 printWelcomeMsg =
   putStrLn "-- P is Player\n\
            \-- C is Computer\n\
            \-- Player is odds, computer is evens.\n\
-           \-- There are five rounds.\n\
-           \-- In each round, choose either one or two.\n\
+           \-- In each round, choose either 1 or 2.\n\
            \-- The computer will do the same, and if the sum\n\
-           \-- is odd you win. If it is even, the computer wins."
+           \-- is odd you win. If it is even, the computer wins.\n\
+           \-- Best of five rounds! Go!"
+
+printWinMsg :: GameState -> IO ()
+printWinMsg (GameState p c r) = do
+  putStrLn "\n ---------------------------"
+  case compare p c of
+    GT -> putStrLn "|    YOU WON! WOHOO!! 😃    |"
+    LT -> putStrLn "|  YOU LOST. YOU SUCK!! 😂  |"
+    EQ -> putStrLn "| It was a tie! Boring.. 😒 |"
+  putStrLn " ---------------------------"
+  putStrLn $ "Player total points: " ++ show p
+  putStrLn $ "Computer total points: " ++ show c
--- src/Morra.hs
+++ src/Morra.hs
@@ -1,1 +1,55 @@
 module Morra where
+import           Control.Applicative       (empty, liftA2, (<|>))
+import           Control.Monad             (unless)
+import           Control.Monad.IO.Class    (liftIO)
+import           Control.Monad.Trans.State
+import           System.Random             (randomRIO)
+
+data GameState =
+  GameState { playerPoints  :: Int
+           , computerPoints :: Int
+           , roundNum       :: Int }
+  deriving (Eq, Show)
+
+gameOver :: GameState -> Bool
+gameOver (GameState p c r) =
+  r > 5 || max p c > 5`div`2
+
+readPlayerInput :: IO Int
+readPlayerInput = do
+  n <- readLn
+  if n `elem` [1,2]
+    then return n
+    else empty
+
+getPlayerNum :: IO Int
+getPlayerNum = putStr "P: " >> readPlayerInput
+  <|> (putStrLn "Please insert either 1 or 2." >> getPlayerNum)
+
+getComputerNum :: IO Int
+getComputerNum = do
+  num <- randomRIO (1,2)
+  putStrLn $ "C: " ++ show num
+  return num
+
+finishRound :: Int -> StateT GameState IO ()
+finishRound sum' = do
+  GameState p c r <- get
+  if odd sum'
+    then do liftIO $ putStrLn "You won the round!"
+            put $ GameState (p+1) c (r+1)
+    else do liftIO $ putStrLn "Computer won the round."
+            put $ GameState p (c+1) (r+1)
+
+round' :: StateT GameState IO ()
+round' = do
+  gameState <- get
+  liftIO $ putStrLn ("\n----ROUND " ++ show (roundNum gameState) ++ "----")
+  (pNum, cNum) <- liftIO $
+    liftA2 (,) getPlayerNum getComputerNum
+  finishRound (pNum+cNum)
+
+game :: StateT GameState IO ()
+game = do round'
+          gameState <- get
+          unless (gameOver gameState) game
