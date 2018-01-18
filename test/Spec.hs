d8b4da3554fe3751f019958385d07fea9bb6f01e
Oskar Ek <oskarek24@gmail.com>
2018-01-18 21:43:10 +0100

    added a test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,8 @@
+import           Morra
+import           Test.Hspec
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $
+  describe "gameOver" $
+    it "is game over when player has 3p" $
+      shouldBe (gameOver (GameState 3 1 4)) True
