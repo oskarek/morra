81a3081af54b45eb0a126eb550ffbbc85e502001
Oskar Ek <oskarek24@gmail.com>
2018-01-18 21:48:19 +0100

    added a new test

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,6 +3,8 @@ import           Test.Hspec
 
 main :: IO ()
 main = hspec $
-  describe "gameOver" $
+  describe "gameOver" $ do
     it "is game over when player has 3p" $
-      shouldBe (gameOver (GameState 3 1 4)) True
+      gameOver (GameState 3 1 4) `shouldBe` True
+    it "is not game over when both have 2p" $
+      gameOver (GameState 2 2 4) `shouldBe` False
